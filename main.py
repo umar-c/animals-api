00c5925833caaf4a3d6bdc81990a0f12764ed128
umar-c <60947368+umar-c@users.noreply.github.com>
2024-06-21 20:44:02 +0000

    Fixed bugs. Addedd comments.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import asyncio
 import aiohttp
 import json
-from datetime import datetime
+from datetime import datetime, timezone
 
 API_URL = "http://localhost:3123/animals/v1"
 BATCH_SIZE = 100
@@ -14,10 +14,13 @@ def fetch_animal_ids():
     page = 1
     while True:
         response = requests.get(f"{API_URL}/animals?page={page}")
+        print(f"response.status_code = {response.status_code}")
         if response.status_code != 200:
-            return
+            return # Return 'None'. Should retry or at least send an empty list.
         data = response.json()
+        print(f"data = {data}")
         ids.extend([animal["id"] for animal in data["items"]])
+        print(f"ids = {ids}")
         if len(data) < 10:  # Assuming the page size is 10
             break
         page += 1
@@ -25,8 +28,18 @@ def fetch_animal_ids():
 
 
 def transform_animal(animal):
+    print(f"animal = {animal}")
     if "friends" in animal:
         animal["friends"] = animal["friends"].split(",")
+    if "born_at" in animal:
+        if animal["born_at"] is not None:
+            print(animal["born_at"])
+            ts_epoch = animal["born_at"]
+            ts = datetime.fromtimestamp(12456).strftime('%Y-%m-%d %H:%M:%S')#, #timezone.utc)
+            #ts = datetime.fromtimestamp(ts, timezone.utc)
+            #animal["born_at"] = datetime.fromtimestamp(4587216, )
+            #print(animal["born_at"])
+            print(ts)
     return animal
 
 
@@ -68,22 +81,27 @@ async def post_animals(session, animals):
 
 
 async def main():
-    animal_ids = fetch_animal_ids()
-
+    print("Hello\n")
+    animal_ids = fetch_animal_ids() 
+    # Should check if animal_ids is None.
+    print(f"animal ids fetched. animal_ids = {animal_ids}\n")
     async with aiohttp.ClientSession() as session:
         animals = []
         for animal_id in animal_ids:
             try:
                 animal = await fetch_animal_detail(session, animal_id)
+                print(f"animal = {animal}")
                 animals.append(animal)
                 if len(animals) == BATCH_SIZE:
                     await post_animals(session, animals)
+                    print(f"animals posted in batch = {animals}")
                     animals = []
             except Exception as e:
                 print(f"Failed to process animal {animal_id}: {e}")
 
         if animals:
             await post_animals(session, animals)
+            print(f"remaining animals posted = {animals}")
 
 
 if __name__ == "__main__":
